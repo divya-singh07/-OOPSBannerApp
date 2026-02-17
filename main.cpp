e387bf6b2f3fdd55a37b42c6066b3b7c8df1ba8c
divya-singh07 <divyasjadoun@gmail.com>
2026-02-17 10:23:09 +0530

    Coded UC2 to print OOPS in Banner Format

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-    cout << "Welcome to OOPS Banner App!" << endl;
+
+    cout << " *****    *****    *****    ***** " << endl;
+    cout << "*     *  *     *  *     *  *     " << endl;
+    cout << "*     *  *     *  *****    *****  " << endl;
+    cout << "*     *  *     *  *             * " << endl;
+    cout << " *****    *****   *        *****  " << endl;
+
     return 0;
 }
